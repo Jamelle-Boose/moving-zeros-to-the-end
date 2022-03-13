0e4b2ad28832f0ec60f3ae1947c6c817c5d7ebb9
Jamelle Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2022-03-13 08:43:55 -0700

    refactor: spread values into array

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 exports.moveZeros = arr => {
-  try {
-    const filterArr = arr.filter(el => el !== 0)
-    const zeroArr = arr.filter(el => el === 0)
-    return [...filterArr, ...zeroArr]
-  } catch (error) {
-    throw error
-  }
+  const filterArr = arr.filter(el => el !== 0)
+  const zeroArr = arr.filter(el => el === 0)
+
+  return [...filterArr, ...zeroArr]
 }
